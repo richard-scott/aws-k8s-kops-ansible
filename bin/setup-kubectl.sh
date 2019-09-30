6c15ad2fda6218a465f0116db5410b774a551982
Richard Scott <git@pointb.co.uk>
2019-09-30 09:46:38 +0100

    updates

--- bin/setup-kubectl.sh
+++ bin/setup-kubectl.sh
@@ -2,7 +2,7 @@
 cd $(dirname ${0})
 cd ..
 if [[ -n "$(which kubectl 2>/dev/null)" ]]; then
-  if $(pwd)/bin/run-role.sh kubectl-export-config ; then
+  if $(pwd)/bin/run-role.sh kubectl-export-config 2>/dev/null ; then
     kubectl cluster-info
   fi
 fi
