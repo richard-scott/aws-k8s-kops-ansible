818db60c5d54b0452a7b7add663bdc5a8f9bd189
Richard Scott <git@pointb.co.uk>
2019-10-11 14:22:13 +0100

    now

--- bin/get-token.sh
+++ bin/get-token.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-kubectl -n kubernetes-dashboard describe secrets $(kubectl -n kubernetes-dashboard get secret | grep "admin-user-token" | awk {'print $1'}) | grep "token:" | awk {'print $2'}
+for NAMESPACE in kube-system kubernetes-dashboard ; do
+  TOKEN_ID="$(kubectl -n ${NAMESPACE} get secret | grep "admin-user-token" | awk {'print $1'})"
+  if [[ -n "${TOKEN_ID}" ]]; then
+    kubectl -n ${NAMESPACE} describe secrets ${TOKEN_ID} #-o 'jsonpath={.data.token}'
+    break
+  fi
+done
+#kubectl get secret -n kube-system | grep k8sadmin | cut -d " " -f1 | xargs -n 1 | xargs kubectl get secret  -o 'jsonpath={.data.token}' -n kube-system | base64 --decode
+
--- bin/run-role.sh
+++ bin/run-role.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd $(dirname $(dirname ${0}))
+pwd
 /usr/bin/ansible localhost -m include_role -a name="$(echo ${1} | sed -r s'|^(roles/)?(.*)/|\2|g')"
