e9dc55af03848a43001169f52241eb9973d1e536
Richard Scott <git@pointb.co.uk>
2019-10-11 15:07:28 +0100

    updates

--- bin/get-token.sh
+++ bin/get-token.sh
@@ -2,9 +2,7 @@
 for NAMESPACE in kube-system kubernetes-dashboard ; do
   TOKEN_ID="$(kubectl -n ${NAMESPACE} get secret | grep "admin-user-token" | awk {'print $1'})"
   if [[ -n "${TOKEN_ID}" ]]; then
-    kubectl -n ${NAMESPACE} describe secrets ${TOKEN_ID} #-o 'jsonpath={.data.token}'
+    echo "$(kubectl -n ${NAMESPACE} get secret ${TOKEN_ID} -o 'jsonpath={.data.token}' | base64 --decode)"
     break
   fi
 done
-#kubectl get secret -n kube-system | grep k8sadmin | cut -d " " -f1 | xargs -n 1 | xargs kubectl get secret  -o 'jsonpath={.data.token}' -n kube-system | base64 --decode
-
