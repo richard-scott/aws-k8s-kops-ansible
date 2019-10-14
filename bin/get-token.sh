031d5aba99d74fc0af496796982a5635709a1e24
Richard Scott <git@pointb.co.uk>
2019-10-14 11:27:20 +0100

    updates

--- bin/get-token.sh
+++ bin/get-token.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-for NAMESPACE in kube-system kubernetes-dashboard ; do
-  TOKEN_ID="$(kubectl -n ${NAMESPACE} get secret | grep "admin-user-token" | awk {'print $1'})"
-  if [[ -n "${TOKEN_ID}" ]]; then
-    echo "$(kubectl -n ${NAMESPACE} get secret ${TOKEN_ID} -o 'jsonpath={.data.token}' | base64 --decode)"
-    break
-  fi
-done
+NAMESPACE=kubernetes-dashboard
+TOKEN_ID="$(kubectl -n ${NAMESPACE} get secret | grep "admin-user-token" | awk {'print $1'})"
+if [[ -n "${TOKEN_ID}" ]]; then
+  echo "$(kubectl -n ${NAMESPACE} get secret ${TOKEN_ID} -o 'jsonpath={.data.token}' | base64 --decode)"
+fi
--- bin/run-role.sh
+++ bin/run-role.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $(dirname ${0}))
 pwd
-/usr/bin/ansible localhost -m include_role -a name="$(echo ${1} | sed -r s'|^(roles/)?(.*)/|\2|g')"
+/usr/bin/ansible localhost -m include_role -a name="$(echo ${1} | sed -r s'|^(roles/)?(.*)/?|\2|g')"
