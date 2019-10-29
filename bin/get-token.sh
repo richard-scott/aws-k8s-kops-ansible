914cbc795d4f8fed27cb366764b47252e17ad344
Richard Scott <github@pointb.co.uk>
2019-10-29 11:17:13 +0000

    updates

--- bin/get-token.sh
+++ bin/get-token.sh
@@ -2,5 +2,5 @@
 NAMESPACE=kubernetes-dashboard
 TOKEN_ID="$(kubectl -n ${NAMESPACE} get secret | grep "admin-user-token" | awk {'print $1'})"
 if [[ -n "${TOKEN_ID}" ]]; then
-  echo "$(kubectl -n ${NAMESPACE} get secret ${TOKEN_ID} -o 'jsonpath={.data.token}' | base64 --decode)"
+  kubectl -n ${NAMESPACE} describe secret ${TOKEN_ID} | egrep "^token:\s+" | awk '{print $2}'
 fi
