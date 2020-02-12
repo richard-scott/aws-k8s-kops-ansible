f7f02dc5893c6a388ad15aacbe0770857d3443c6
Richard Scott <github@pointb.co.uk>
2020-02-12 14:08:29 +0000

    Updates

--- bin/remove-namespace.sh
+++ bin/remove-namespace.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+for CHART in $(helm list -a | egrep "\s${1}$" | awk '{print $1}') ; do
+  helm del --purge ${CHART}
+done
 for RESOURCES in ingress service deployment configmap ; do
   for RESOURCE in $(kubectl get ${RESOURCES} -n ${1} --no-headers | awk '{print $1}') ; do
-    kubectl delete ${RESOURCES} -n ${1} ${RESOURCE}
-    for CHART in $(helm list -a | egrep "\s${1}$" | awk '{print $1}') ; do
-      helm del --purge ${CHART}
-    done
+    if ! kubectl describe service ${RESOURCE} -n ${1} | egrep -q "provider=kubernetes"; then
+      kubectl delete ${RESOURCES} -n ${1} ${RESOURCE}
+    fi
   done
 done
