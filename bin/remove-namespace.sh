25d8b4b990000482ccc163a571be3d259a85cd00
Richard Scott <github@pointb.co.uk>
2020-02-26 16:51:22 +0000

    Updates

--- bin/remove-namespace.sh
+++ bin/remove-namespace.sh
@@ -9,3 +9,8 @@ for RESOURCES in ingress service deployment configmap ; do
     fi
   done
 done
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
