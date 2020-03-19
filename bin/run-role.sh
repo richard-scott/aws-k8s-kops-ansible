0e61bf7571b2edeb2a3009c3c7ae0dd39d484c65
Richard Scott <github@pointb.co.uk>
2020-03-19 17:14:14 +0000

    Updates

--- bin/run-role.sh
+++ bin/run-role.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 cd $(dirname $(dirname ${0}))
-/usr/bin/ansible localhost -m include_role -a name="$(echo ${1} | sed -r s'|^(roles/)?(.*)/?|\2|g')"
+ROLE="$(echo ${1} | sed -r s'|^(roles/)?(.*)/?$|\2|g' | sed -r 's|/$||g')"
+/usr/bin/ansible localhost -m include_role -a name=${ROLE} | tee logs/${ROLE}.log
